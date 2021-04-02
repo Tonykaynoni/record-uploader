247bfab98a91731d33241d4fe655c38c42bca4ca
Tonkay-Noni <tonykay001@gmail.com>
2021-04-02 09:42:01 +0300

    Update on structure

--- app/Http/Controllers/CustomerDetailsController.php
+++ app/Http/Controllers/CustomerDetailsController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\UploadCustomersDetailsProcess;
-use Carbon\Carbon;
-use Throwable;
 
 //
 
@@ -17,47 +15,14 @@ class CustomerDetailsController extends Controller
 
     }
 
-    public function checkDate()
-    {
-        $age = 0;
-
-        try {
-            $date = date_create("1955-12-05 00:00:00");
-            $newDate = date_format($date, "Y/d/m H:i:s");
-            // $newDate = $date->format("Y/d/m");
-
-            $date1 = Carbon::parse('now');
-            $date2 = Carbon::createMidnightDate(Carbon::parse($newDate)->toDateTimeString());
-            $age = $date1->diffInYears($date2);
-        } catch (Throwable $e) {
-            return $age;
-        }
-
-        return $age;
-    }
-    public function dateFormat()
-    {
-        $date_format['d.m.Y'] = 'd.m.Y';
-        $date_format['d.m.y'] = 'd.m.y';
-        $date_format['d-m-Y'] = 'd-m-Y';
-        $date_format['d-m-y'] = 'd-m-y';
-        $date_format['d/m/Y'] = 'd/m/Y';
-        $date_format['d/m/y'] = 'd/m/y';
-
-        return $date_format;
-    }
-
     public function upload()
     {
         $data = json_decode(file(request()->myJsonRecord)[0], true);
-        // $data = file(request()->myJsonRecord)[0];
-        //Chunking file
 
+        //Chunking file
         $chunk = array_chunk($data, 100);
-        // dd($chunk);
         $uniqueKey = 0;
         foreach ($chunk as $key => $value) {
-
             $name = "/tmp{$uniqueKey}.json";
             $path = resource_path('temp');
             file_put_contents($path . $name, json_encode($value));
@@ -70,10 +35,11 @@ class CustomerDetailsController extends Controller
 
     public function storeData()
     {
-
+        // Read chucked file from directory
         $path = resource_path('temp');
         $files = glob("$path/*.json");
 
+        // Run queue the files data for backgroud job
         foreach ($files as $file) {
             $data = json_decode(file($file)[0], true);
             UploadCustomersDetailsProcess::dispatch($data);
