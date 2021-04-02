c2f9fd77536b2741a941f3a50a6a9180deb4d3ab
Tonkay-Noni <tonykay001@gmail.com>
2021-04-02 10:02:37 +0300

    Update on structure

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use App\Http\Controllers\CustomerDetailsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [CustomerDetailsController::class, 'index']);
-
 Route::post('/upload', [CustomerDetailsController::class, 'upload']);
 Route::get('/store-data', [CustomerDetailsController::class, 'storeData']);
-Route::get('/test-data', [CustomerDetailsController::class, 'checkDate']);
