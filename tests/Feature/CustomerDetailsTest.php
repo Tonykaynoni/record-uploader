611296e42c79ab1e158f853be0a1958086c8836a
Tonkay-Noni <tonykay001@gmail.com>
2021-04-02 10:37:35 +0300

    Update on structure

--- tests/Feature/CustomerDetailsTest.php
+++ tests/Feature/CustomerDetailsTest.php
@@ -2,21 +2,9 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CustomerDetailsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
 }
